c82a0cb9296434dde9bf42b591eba3e7f278697b
mariahedkvist <mariasofiahedkvist@gmail.com>
2022-09-30 11:16:40 +0200

    utan kommentarer

--- script.js
+++ script.js
@@ -7,46 +7,58 @@ const sidebarDiv = document.getElementById('sidebar');
 const failedLogin = document.getElementById('failedLogin');
 const btnLogin = document.getElementById('btnLogin');
 
-btnLogin.onclick = function storeLogin() {
-    let inputUsername = document.getElementById('username').value;
-    let inputPassword = document.getElementById('password').value;
-    localStorage.setItem('activeUser', inputUsername);
+homepage();
 
-    if (inputUsername == checkUsername && inputPassword == checkPassword) {
+function homepage() {
+    btnLogin.onclick = function storeLogin() {
+        let inputUsername = document.getElementById('username').value;
+        let inputPassword = document.getElementById('password').value;
+        localStorage.setItem('activeUser', inputUsername);
+
+        if (inputUsername == checkUsername && inputPassword == checkPassword) {
+            activeUserPage();
+        }
+        else {
+            wrongPasswordAlert();
+        }
+    }
+
+    if (localStorage.getItem('activeUser') == 'Sara') {
         activeUserPage();
     }
-    else {
-        wrongPasswordAlert();
-    }
-}
 
-if (localStorage.getItem('activeUser') == 'Sara') {
-    activeUserPage();
-}
+    function activeUserPage() {
+        loginForm.style.display = 'none';
+        failedLogin.style.display = 'none';
 
-function activeUserPage() {
-    loginForm.style.display = 'none';
-    failedLogin.style.display = 'none';
+        const welcomeText = document.createElement('h3');
+        welcomeText.innerHTML = "Välkommen " + localStorage.getItem('activeUser');
+        sidebarDiv.appendChild(welcomeText);
 
-    const welcomeText = document.createElement('h3');
-    welcomeText.innerHTML = "Välkommen " + localStorage.getItem('activeUser');
-    sidebarDiv.appendChild(welcomeText);
-
-    const btnLogout = document.createElement('button');
-    btnLogout.setAttribute('id', 'btnLogout');
-    btnLogout.innerHTML = "Logga ut";
-    sidebarDiv.appendChild(btnLogout);
-    btnLogout.onclick = function logout() {
-        localStorage.clear();
-        btnLogout.style.display = 'none';
-        welcomeText.style.display = 'none';
-        loginForm.style.display = 'block';
+        const btnLogout = document.createElement('button');
+        btnLogout.setAttribute('id', 'btnLogout');
+        btnLogout.innerHTML = "Logga ut";
+        sidebarDiv.appendChild(btnLogout);
+        btnLogout.onclick = function logout() {
+            localStorage.clear();
+            btnLogout.style.display = 'none';
+            welcomeText.style.display = 'none';
+            loginForm.style.display = 'block';
+        }
     }
 }
 
 function wrongPasswordAlert() {
+    loginForm.style.display = 'none';
+
     const wrongPasswordMessage = document.createElement('p');
-    wrongPasswordMessage.innerHTML = "Fel lösenord, försök igen";
+    wrongPasswordMessage.innerHTML = "Fel lösenord";
     failedLogin.appendChild(wrongPasswordMessage);
+
+    const tryagainBtn = document.createElement('button');
+    tryagainBtn.innerHTML = "Försök igen";
+    failedLogin.appendChild(tryagainBtn);
     localStorage.clear();
+    
+    tryagainBtn.onclick() = homepage();
 }
